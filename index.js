5f77aeeb6bf5c2079100b7a9af122f88894ba93a
belloshe <isabelle.ibrahimsson@edu.cmeducations.se>
2023-05-13 23:04:38 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const characters = require("./routes/characters");
+const cors = require("cors");
+
+const app = express();
+const port = 3001;
+
+// Configure CORS policy
+const corsOptions = {
+  origin: "http://localhost:3000", // Only allow requests from localhost:3000
+};
+
+// Apply CORS middleware to all routes
+app.use(cors(corsOptions));
+
+// Använd JSON-middleware
+app.use(express.json());
+
+const validApiKey = "5";
+
+const authenticateApiKey = (req, res, next) => {
+  const apiKey = req.query.apiKey;
+
+  if (!apiKey) {
+    return res
+      .status(401)
+      .json({ message: "Inga karaktärer till dig! Din api key saknas." });
+    // returnera 401
+  }
+
+  if (apiKey !== validApiKey) {
+    return res.status(403).json({ message: "Invalid API key" });
+  }
+
+  next();
+};
+
+app.use((req, res, next) => {
+  authenticateApiKey(req, res, next);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use("/characters", characters);
+
+// Starta servern på angiven port
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
